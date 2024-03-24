47967dfd93b42043c795aa5ae2ef6666346abcfa
Adegbehingbe, Oluwakorede Joseph <oluwakoredeadegbehingbe@gmail.com>
2024-03-25 00:54:24 +0100

    Setup workmanager for background tasks

--- lib/screens/onboarding/splash_screen.dart
+++ lib/screens/onboarding/splash_screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:time_guard/screens/wrapper.dart';
-import 'package:time_guard/services/background_service.dart';
+// import 'package:time_guard/services/background_service.dart';
 import 'package:time_guard/shared/constants.dart';
 import 'package:time_guard/shared/utils/navigator.dart';
 
@@ -22,7 +22,7 @@ class SplashScreen extends StatelessWidget {
           width: 200.w,
         ).animate(
           onComplete: (controller) async {
-            await BackgroundService().initPlatformState(context);
+            // await BackgroundService().initPlatformState(context);
             navigatorPushReplacement(context, const Wrapper());
           },
         )
